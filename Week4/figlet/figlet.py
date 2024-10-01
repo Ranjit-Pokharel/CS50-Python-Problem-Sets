51a43429071f33daa9123822f05a5c8548b39c0e
Ranjit-Pokharel <rp.ranjitpokharel.pr@gmail.com>
2024-10-01 22:03:08 +0545

    Tue Oct  1 10:03:08 PM +0545 2024

--- Week4/figlet/figlet.py
+++ Week4/figlet/figlet.py
@@ -3,34 +3,39 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 
+def main():
+    # geting argument excluding the script
+    argv = sys.argv[1:]
+    
+    # checking program.py [argument] optional valid or not
+    error_check(argv)
 
-def main(argv):
-    if set_figlet_font(argv):
-        render_message()
+    # seting the fonts
+    if len(argv) == 0:
+        figlet.setFont(font = random.choice(figlet.getFonts()))
     else:
-        sys.exit("Invalid usage")
+        figlet.setFont(font = argv[1])
 
-
-
-def set_figlet_font(argv):
-    font_list = figlet.getFonts()
-    is_font_set = False
-    if len(argv) == 1:
-        figlet.setFont(font = font_list[random.randint(0, 424)])
-        is_font_set = True
-    elif len(argv) == 3:
-        if argv[1] == "-f" or argv[1] == "--font":
-            if argv[2] in font_list:
-                figlet.setFont(font = argv[2])
-                is_font_set = True
-
-    return is_font_set
-
-
-def render_message():
+    # asking user for text
     message = input("Input: ")
+
+    # display text with font
     print("Output:", figlet.renderText(message), sep="\n")
 
 
+def error_check(argv):
+    length = len(argv)
+
+    if length not in [0, 2]:
+        sys.exit("Invalid usage")
+
+    if length == 2:
+        if argv[0] not in ["-f", "--font"]:
+            sys.exit("Invalid usage")
+        if argv[1] not in figlet.getFonts():
+            sys.exit("Invalid usage")
+        
+
+
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
