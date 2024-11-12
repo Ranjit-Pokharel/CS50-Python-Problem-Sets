f928280c3b48f7d89a2fb2089474e031046fd2cd
Ranjit-Pokharel <rp.ranjitpokharel.pr@gmail.com>
2024-11-12 17:53:24 +0545

    Tue Nov 12 05:53:24 PM +0545 2024

--- psets1/meal/meal.py
+++ psets1/meal/meal.py
@@ -1,20 +1,62 @@
-def main():
+"""
+psets1/meal
+-----------
+    a program that prompts the user for a time 
+    and outputs whether it's breakfast time, 
+    lunch time, or dinner time. 
+    If it's not time for a meal, 
+    don't output anything at all. 
+
+Formatted
+---------
+    in 24-hour time as #:## or ##:##. 
+    And assume that each meal's time range is inclusive. 
+    For instance, whether it's 7:00, 7:01, 7:59, or 8:00, 
+    or anytime in between, it's time for breakfast.
+"""
+
+
+def main() -> None:
+    """
+    get time from user and evaluate the time
+    if dinner or lunch or breakfast
+
+    Parameters
+    ----------
+    - None
+
+    Returns
+    -------
+    - None
+    """
     # ask user for time
-    time = input("What time is it? ")
+    time: str = input("What time is it? ")
 
     # convert the time
     hours = convert(time)
 
     # check time to display the meal type
-    if hours >= 7 and hours <= 8:
+    if 7 <= hours <= 8:
         print("breakfast time")
-    elif hours >= 12 and hours <= 13:
+    elif 12 <= hours <= 13:
         print("lunch time")
-    elif hours >= 18 and hours <= 19:
+    elif 18 <= hours <= 19:
         print("dinner time")
 
 
-def convert(time):
+def convert(time: str) -> float:
+    """
+    evaluate the time provided
+
+    Parameters
+    ----------
+    - time (str):
+        the time that need evaluation
+
+    Returns
+    -------
+    - float: result after evaluation
+    """
     hrs, min = time.split(":")
 
     if "a.m" in min:
